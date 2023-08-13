ae125394a6f7c616827b62f6162457aa77b439ae
Andrey <andrey291188@gmail.com>
2023-08-13 23:56:01 +0300

    UP

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,20 +1,15 @@
-// import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import css from '../styles.module.css';
 import { useEffect } from 'react';
 
-
-// const modalRoot = document.querySelector('#modal-root');
-
-const Modal = ({imgModal, toggleModal}) => {
-
-  useEffect(()=>{
+const Modal = ({ imgModal, toggleModal }) => {
+  useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
 
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
-    }
-  },[])
+    };
+  }, []);
 
   const handleKeyDown = e => {
     if (e.code === 'Escape') {
@@ -28,50 +23,14 @@ const Modal = ({imgModal, toggleModal}) => {
     }
   };
 
-    return (
-      <div className={css.Overlay} onClick={backdropClick}>
-        <div className={css.Modal}>
-          <img src={imgModal.largeImageURL} alt={imgModal.tags} />
-        </div>
-      </div>);
+  return (
+    <div className={css.Overlay} onClick={backdropClick}>
+      <div className={css.Modal}>
+        <img src={imgModal.largeImageURL} alt={imgModal.tags} />
+      </div>
+    </div>
+  );
 };
-
-
-
-// class Modal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handleKeyDown);
-
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   handleKeyDown = e => {
-//     if (e.code === 'Escape') {
-//       this.props.toggleModal();
-//     }
-//   };
-
-//   backdropClick = e => {
-//     if (e.currentTarget === e.target) {
-//       this.props.toggleModal();
-//     }
-//   };
-
-//   render() {
-//     const { imgModal } = this.props;
-//     return createPortal(
-//       <div className={css.Overlay} onClick={this.backdropClick}>
-//         <div className={css.Modal}>
-//           <img src={imgModal.largeImageURL} alt={imgModal.tags} />
-//         </div>
-//       </div>,
-//       modalRoot
-//     );
-//   }
-// }
 
 export default Modal;
 
@@ -81,4 +40,4 @@ Modal.propTypes = {
     tags: PropTypes.string.isRequired,
   }).isRequired,
   toggleModal: PropTypes.func.isRequired,
-}
+};
